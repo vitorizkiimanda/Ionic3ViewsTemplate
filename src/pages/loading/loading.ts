17e40978c74781db5b854988924b9ecb61f9c04c
Vito Rizki Imanda <vitorizkiimanda@gmail.com>
2019-04-16 17:13:46 +0700

    generated new pages

--- src/pages/loading/loading.ts
+++ src/pages/loading/loading.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, LoadingController } from 'ionic-angular';
 
 /**
  * Generated class for the LoadingPage page.
@@ -15,11 +15,19 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class LoadingPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams,public loadingCtrl: LoadingController) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad LoadingPage');
+  }
+
+  bukaLoading() {
+    const loader = this.loadingCtrl.create({
+      content: "Harap tunggu...",
+      duration: 3000
+    });
+    loader.present();
   }
 
 }
